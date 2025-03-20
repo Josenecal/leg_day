eec3aaf154f5940cae1cb294c05f91bedd159ffd
Joseph Senecal <jasenecal2@gmail.com>
2025-03-20 14:35:15 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
-has_secure_password
+    [:first_name, :last_name, :email, :password].each do |required_attr|
+        validates required_attr, presence: true
+    end
+
+    validates :email, uniqueness: true
+    
+    has_secure_password
 
 end
