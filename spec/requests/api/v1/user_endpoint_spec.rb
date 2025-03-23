e44e7d4228b51e230596f9a4a3f141b5e6b65b94
Joseph Senecal <jasenecal2@gmail.com>
2025-03-22 22:51:02 -0600

    Outline user creation request specs

--- spec/requests/api/v1/user_endpoint_spec.rb
+++ spec/requests/api/v1/user_endpoint_spec.rb
@@ -1,10 +1,12 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe '/api/v1/user' do
     context '/create endpoint' do
         it 'creates a user when given valid data' do
-            # expect response code to be 201
-            # expect response body to contain created user parameters
+            post "/api/v1/users", params: {first_name: "Bob", last_name: "Belcher", email: "bob@bobsburgers.com", password: "password"}, headers: {accept: "application/json", content_type: "application/json"}
+            
+            expect(response.code).to eq "200"
         end
 
         it 'responds with a 400 code if missing required data' do
