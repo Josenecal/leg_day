76ea81598decf381d018ee1598984968533a39cf
Joseph Senecal <jasenecal2@gmail.com>
2025-03-28 08:46:35 -0600

    refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,20 @@
 class User < ApplicationRecord
-    [:first_name, :last_name, :email, :password_digest].each do |required_attr|
-        validates required_attr, presence: true
+    REQUIRED_FIELDS = [:first_name, :last_name, :email, :password_digest]
+    UPDATABLE_FIELDS = []
+
+    REQUIRED_FIELDS.each do |attr|
+        validates attr, presence: true
     end
 
     validates :email, uniqueness: true
     
     has_secure_password
 
+    def self.new_record_params()
+        REQUIRED_FIELDS
+    end
+
+    def self.updatable_params()
+        UPDATABLE_FIELDS
+    end
 end
