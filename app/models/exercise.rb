5b596493ab4ac637654a4094bc6ac135eeea5824
Joseph Senecal <jasenecal2@gmail.com>
2025-03-28 08:46:57 -0600

    Create workout, exercise models

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,2 +1,14 @@
 class Exercise < ApplicationRecord
+    REQUIRED_FIELDS = []
+    UPDATABLE_FIELDS = []
+
+    has_many :workouts, through: :workouts_exercises
+
+    def self.new_record_params()
+        REQUIRED_FIELDS
+    end
+
+    def self.updatable_params()
+        UPDATABLE_FIELDS
+    end
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,12 @@
 class Workout < ApplicationRecord
+    REQUIRED_FIELDS = []
+    UPDATABLE_FIELDS = []
+
+    def self.new_record_params()
+        REQUIRED_FIELDS
+    end
+
+    def self.updatable_params()
+        UPDATABLE_FIELDS
+    end
 end
