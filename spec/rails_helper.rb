5c51b41d07f225d3b0d241650c633ed6d6d63625
Joseph Senecal <jasenecal2@gmail.com>
2025-03-29 12:31:17 -0600

    Initialize simplecov with RSpec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,4 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+
+# Initialize simplecov:
+require 'simplecov'
+SimpleCov.start
+
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
