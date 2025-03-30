dfe5ce799ee9aeb0030f2ded7e7ddb0a9288525e
Joseph Senecal <jasenecal2@gmail.com>
2025-03-30 10:37:37 -0600

    Fix unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :update, :destroy]
-      resources :workouts
+      resources :workouts, only: [:index, :create, :update, :destroy]
     end
   end
 
