b0cc518f4b93a7bca2eb32b2abe2988c50f529c3
Joseph Senecal <jasenecal2@gmail.com>
2025-03-29 19:13:59 -0600

    Test SetStructure db columns and associations

--- spec/models/set_structure_spec.rb
+++ spec/models/set_structure_spec.rb
@@ -1,3 +1,19 @@
-RSpec.describe SetStructure, type: :model do
+require 'rails_helper'
 
+RSpec.describe SetStructure, type: :model do
+    context 'associations' do
+        it { should belong_to(:workout) }
+        it { should belong_to(:exercise) }
+      end
+    
+      context 'validations' do
+        # Placeholder - No current validations
+      end
+    
+      context 'database table' do
+        it { should have_db_column(:workout_id) }
+        it { should have_db_column(:exercise_id) }
+        it { should have_db_column(:sets).of_type(:integer) }
+        it { should have_db_column(:reps).of_type(:integer) }
+      end
 end
