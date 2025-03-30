619bf7c7e776577ab428f94452691505f49cd569
Joseph Senecal <jasenecal2@gmail.com>
2025-03-30 10:54:07 -0600

    Create workouts controller

---
+++ app/controllers/api/v1/workouts_controller.rb
@@ -0,0 +1,21 @@
+class WorkoutsController < ApplicationController
+    def index()
+        # Do stuff
+    end
+
+    def show()
+        # Do stuff
+    end
+
+    def create()
+        # Do stuff
+    end
+
+    def update()
+        # Do stuff
+    end
+
+    def destroy()
+        # Do stuff
+    end
+end
