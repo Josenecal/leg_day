bda88a4e5c1b7394f746083059db4d0c187bf14a
Joseph Senecal <jasenecal2@gmail.com>
2025-04-01 13:35:13 -0600

    Fix potential bad id parsing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::API
     def current_user()
         # TO-DO: This will eventually implement JWT auth and need
         # to be updated to reflect this.
-        # require 'pry'; binding.pry
         user_id = request.headers['Authorization']
-        unless user_id.present?
+        unless user_id.present? && user_id.match?(/\A\d+\z/)
             return nil
         else
             return User.find_by(id: user_id.to_i)
