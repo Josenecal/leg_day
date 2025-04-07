2f8fdcf2378d24bc6dcaf5607879e0096ee43136
Joseph Senecal <jasenecal2@gmail.com>
2025-04-07 12:36:24 -0600

    Add resistance to SetStructureSerializer

--- app/serializers/set_structure_serializer.rb
+++ app/serializers/set_structure_serializer.rb
@@ -1,9 +1,16 @@
 class SetStructureSerializer
   include JSONAPI::Serializer
+  
   belongs_to :workout
   belongs_to :exercise
+
   attributes :sets, :reps
+
   attribute :name do |set|
     set.exercise.name
+  end
+
+  attribute :resistance do |set|
+    "#{set.resistance} #{set.resistance_unit}"
   end
 end
