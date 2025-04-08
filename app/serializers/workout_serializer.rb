a86ec05fde3f34675d0cf7ca076e49ce68835438
Joseph Senecal <jasenecal2@gmail.com>
2025-04-08 13:31:37 -0600

    Fix duplicated ID attribute

--- app/serializers/workout_serializer.rb
+++ app/serializers/workout_serializer.rb
@@ -2,7 +2,6 @@ class WorkoutSerializer
   include JSONAPI::Serializer
   has_many :set_structures
   has_many :exercises
-  attributes :id
   attribute :completed_at do |w|
     w.created_at.strftime("%A, %m/%d/%Y, %I:%M%p")
   end
