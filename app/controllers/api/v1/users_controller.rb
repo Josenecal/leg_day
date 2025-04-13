aca397d7d6cd65cf0c9f19ece71e7aa79dac8e0c
Joseph Senecal <jasenecal2@gmail.com>
2025-04-13 13:11:48 -0600

    Feat serialize new user on creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::UsersController < ApplicationController
 
     def create
         new_user = User.new(new_user_params())
-        if new_user.save
-            render status: 201
+        if new_user.save!
+            render json: UserSerializer.new(new_user).serializable_hash, status: 201
         else
             render status: 422
         end
