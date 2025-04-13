c2a90139b1bd5dadeeff59ed2f2d1a10eb1f78d5
Joseph Senecal <jasenecal2@gmail.com>
2025-04-13 00:04:49 -0600

    Fix spec for updated helper

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -5,24 +5,18 @@ RSpec.describe ApplicationController, type: :controller do
     
     context 'helper methods' do
 
-        let! (:existing_user) do
-            User.create(
-                    first_name: "Homer",
-                    last_name: "Simpson",
-                    email: "safety@springfieldnuclear.org",
-                    password: "d'oh!"
-                )
-        end
+        let!(:existing_user)  { create :user }
 
         context '#current_user(id)' do
 
             it 'finds and returns a user when passed a valid user id' do
-
-                expect(controller.current_user(existing_user.id.to_s)).to eq existing_user
+                controller.request.headers["Authorization"] =  "#{existing_user.id}"
+                expect(controller.current_user()).to eq existing_user
             end
 
             it 'returns nil when passed an invalid user ID' do
-                expect(controller.current_user(0)).to eq nil
+                controller.request.headers["Authorization"] = "#{User.last.id + 1}"
+                expect(controller.current_user()).to eq nil
             end
 
             it 'returns nil when passed no user ID' do
