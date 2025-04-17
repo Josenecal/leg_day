a335444ed4a938152f9e1e1dbe3fcad490ff5c23
Joseph Senecal <jasenecal2@gmail.com>
2025-04-17 13:05:50 -0600

    Test add some exercise request specs

--- spec/requests/api/v1/exercises_request_spec.rb
+++ spec/requests/api/v1/exercises_request_spec.rb
@@ -1,24 +1,56 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe "/api/v1/exercises" do
     context "GET /" do
-        context "request headers" do
-            it "requires generally required headers 'accepts' and 'content-type'" do
+        let!(:exercises) { create_list :exercise, 30 }
+        let!(:required_headers) { {"Accept" => "application/json", "Content-Type" => "application/json"} }
 
+        context "request headers" do     
+            it "requires only generally required headers, 'accepts' and 'content-type'" do
+                get "/api/v1/exercises", headers: {"Accept" => "application/json", "Content-Type" => "application/json"}
+
+                expect(response.status).to eq 200
             end
 
-            it "requires no authorization" do
+            it "responds 400 if a required header is missing" do
+                get "/api/v1/exercises", headers: {"Accept" => "application/json"}
+                expect(response.status).to eq 400
+
+                get "/api/v1/exercises", headers: {"Content-Type" => "application/json"}
+                expect(response.status).to eq 400
+
+                get "/api/v1/exercises", headers: {}
+                expect(response.status).to eq 400
+            end
+
+            it "responds 400 if a required header is not set to \"application/json\"" do
+                get "/api/v1/exercises", headers: {"Accept" => "text/html", "Content-Type" => "text/html"}
+                # binding.pry
+                expect(response.status).to eq 400
+            end
+
+            it "includes a helpful error message for header issues" do
 
             end
         end
 
         context "response" do
-            it "returns an index of available exercises" do
-                
+            it "returns a list of exercises" do
+                get "/api/v1/exercises", headers: required_headers
+
+                types = JSON.parse(response.body, symbolize_names: true)[:data].pluck :type
+                types.each do |t|
+                    expect(t).to eq "exercise"
+                end
             end
 
-            it "pagenates results in groups of 10 via query params" do
-                
+            it "pagenates results in groups of 20 via query params" do
+                get "/api/v1/exercises", headers: required_headers
+
+                resource_count = JSON.parse(response.body, symbolize_names: true)[:data].count
+
+                expect(resource_count).to eq 20
             end
         end
 
