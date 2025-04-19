de3a08de19dd0e1cb4ac3bd1d7b856604116e097
Joseph Senecal <jasenecal2@gmail.com>
2025-04-19 11:52:17 -0600

    Feat add exercise seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Seed Exercises from https://github.com/yuhonas/free-exercise-db/blob/main/dist/exercises.json
+
+data = File.read(Rails.root.join("db/seed_data/exercises.json"))
+exercises = JSON.parse(data)
+
+exercises.each do |e|
+    exercise = Exercise.new(
+        name: e[:name],
+        level: e[:level],
+        mechanic: e[:mechanic],
+        equipment: [e[:equipment]],
+        primary_muscles: e[:primaryMuscles],
+        secondary_muscles: e[:secondaryMuscles],
+        instructions: e[:instructions],
+        category: e[:category],
+        json_id: e[:id]
+    )
+end
