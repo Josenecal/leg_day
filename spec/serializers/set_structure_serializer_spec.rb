ce08b3ce3618931ae9307df88285b0f34274f2ad
Joseph Senecal <jasenecal2@gmail.com>
2025-04-19 13:04:57 -0600

    Fix: set_structure serializer spec

--- spec/serializers/set_structure_serializer_spec.rb
+++ spec/serializers/set_structure_serializer_spec.rb
@@ -2,42 +2,19 @@ require 'rails_helper'
 require 'pry'
 
 RSpec.describe WorkoutSerializer do
-    let! (:user) {
-        User.create(
-            first_name: "Leopold",
-            last_name: "Loggle",
-            email: "loggle@wartwoodgeneral.com",
-            password: "G41N5"
-        )
-    }
-    let! (:exercise) {
-        Exercise.create(
-            name: "squat",
-            description: "thighs parallel to the ground",
-            muscle_groups: ["thighs","core","back"],
-            equipment: ["bar", "plate weights", "squat rack"],
-            discipline: "Weight Training",
-            category: 0
-        )
-    }
+    let! (:user) { create :user }
+    let! (:exercise) { create :exercise }
     let! (:workout) {
         Workout.create(
             user_id: user.id,
             created_at: DateTime.new(1970,02,25,20,00)
         )
     }
-    let! (:set_structure) {
-        SetStructure.create(
-            workout_id: workout.id,
-            exercise_id: exercise.id,
-            sets: 3,
-            reps: 12,
-            resistance: 150,
-            resistance_unit: 0
-        )
-    }
+    let! (:set_structure) { create :set_structure, workout_id: workout.id, exercise_id: exercise.id }
+    
     context "serializable_hash data shape" do
         it "should serialize data into the expected JSON format" do
+            actual = SetStructureSerializer.new(set_structure).serializable_hash
             expected = {
                 data: {
                     id: "#{set_structure.id}",
@@ -46,13 +23,12 @@ RSpec.describe WorkoutSerializer do
                         sets: set_structure.sets,
                         reps: set_structure.reps,
                         name: exercise.name,
-                        resistance: "#{set_structure.resistance} lbs"
+                        resistance: "#{set_structure.resistance} #{set_structure.resistance_unit}"
                     }
                 }
             }
-            actual = SetStructureSerializer.new(set_structure).serializable_hash
 
-            expect(expected).to eq actual
+            expect(actual).to eq expected
         end
     end
 end
