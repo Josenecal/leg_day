2bc1d016060e1f7aac8e305ac7bf4affb5f1a1da
Joseph Senecal <jasenecal2@gmail.com>
2025-04-24 13:24:22 -0600

    Test: refactor and comment

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ApplicationController, type: :controller do
 
         let!(:existing_user)  { create :user }
 
-        context '#current_user(id)' do
+        context '#current_user()' do
 
             it 'finds and returns a user when passed a valid user id' do
                 controller.request.headers["Authorization"] =  "#{existing_user.id}"
@@ -23,5 +23,16 @@ RSpec.describe ApplicationController, type: :controller do
                 expect(controller.current_user()).to eq nil
             end
         end
+
+        context '#check_required_headers()' do
+            # Since this method renders a response, it is currently being tested in 
+            # request specs. TODO -> dry this out, maybe with a test-env-only route?
+        end
+
+        context '#authenticate_request()' do
+            # Since this method renders a response, it is currently being tested in 
+            # request specs. TODO -> dry this out, maybe with a test-env-only route?
+        end
+
     end
 end
