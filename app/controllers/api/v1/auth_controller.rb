9877744d7f1f75427f6a46546892b6697e5ef8a4
Joseph Senecal <jasenecal2@gmail.com>
2025-08-04 19:02:05 -0600

    Feat: Issue basic JWT on successful logign. Also streamlined code flow.

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,19 +1,13 @@
+require 'jwt'
 class Api::V1::AuthController < ApplicationController
 
     def login()
         credentials = sanatize_login_info
-
         user = User.find_by(email: credentials["email"])
         if user && user.authenticate(credentials["password"])
-            body = {
-                "status": 200,
-                "code": "OK",
-                "message": "Authentication Successful",
-                "token": "test_test_123" # Fix this and generate dynamically once infra is up
-            }
-            render json: body.to_json
+            render json: successful_response(user).to_json
         else
-            render json: failure_response
+            render json: failure_response().to_json
         end
     end
 
@@ -25,11 +19,29 @@ class Api::V1::AuthController < ApplicationController
 
     def failure_response()
         {
-            "status": 401,
-            "code": "UNAUTHORIZED",
-            "message": "Authentican Failed",
-            "details": "The email and password provided do not match"
-            }.to_json
+            status: 401,
+            code: "UNAUTHORIZED",
+            message: "Authentican Failed",
+            details: "The email and password provided do not match"
+        }
+    end
+
+    def successful_response(user)
+        {
+            status: 200,
+            code: "OK",
+            message: "Authentication Successful",
+            token: generate_token(user)
+        }
+    end
+
+    def generate_token(user)
+        #We need to decide on an encoding strategy, and determine what data to actually encode.
+        payload = user.token_attributes #still need to define this 
+        algorithm = ENV['JWT_STRAT']
+        secret = ENV['JWT_SECRET']
+        
+        JWT.encode(payload, secret, algorithm)
     end
 
 end
