a22b744760409cec440b9338c5647be9e85424f9
Joseph Senecal <jasenecal2@gmail.com>
2025-08-04 19:02:54 -0600

    Feat: new User class method returns user attributes for JWT encoding

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,4 +16,8 @@ class User < ApplicationRecord
     def self.updatable_params()
         return [:first_name, :last_name]
     end
+
+    def self.token_attributes()
+        return [:id] # FOR NOW, decide what else needs to go here and add above if it needs to be derrived 
+    end
 end
