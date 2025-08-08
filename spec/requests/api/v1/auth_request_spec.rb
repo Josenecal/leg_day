fb5653cc5b8465c960142eac2beaf155c5727174
Joseph Senecal <jasenecal2@gmail.com>
2025-08-08 15:54:52 -0600

    Test: successful auth response added request tests for the shapes of the decoded payload and header from the issued JWT

--- spec/requests/api/v1/auth_request_spec.rb
+++ spec/requests/api/v1/auth_request_spec.rb
@@ -12,10 +12,6 @@ RSpec.describe 'api/v1/sessions' do
             }
         }
 
-        context 'authorization' do
-            # Sessions endpoint requires no authorization at this time, so there's nothing to test here
-        end
-
         context 'response' do
             let!(:expected_failure_response) { 
                 {
@@ -26,14 +22,14 @@ RSpec.describe 'api/v1/sessions' do
                 } 
             }
 
-            let!(:expected_successful_response) {
-                {
-                    "status": 200,
-                    "code": "OK",
-                    "message": "Authentication Successful",
-                    "token": nil # force to fail until the JWT infrastructure is up and usable
-                }
-            }
+            # let!(:expected_successful_response) {
+            #     {
+            #         "status": 200,
+            #         "code": "OK",
+            #         "message": "Authentication Successful",
+            #         "token": nil # force to fail until the JWT infrastructure is up and usable
+            #     }
+            # }
 
             let!(:correct_body) {
                 {
@@ -75,7 +71,36 @@ RSpec.describe 'api/v1/sessions' do
             it 'returns 200 if the password is correct' do 
                 post "/api/v1/auth", headers: required_headers, params: correct_body
 
-                expect(JSON.parse(response.body, symbolize_names: true)).to eq expected_successful_response
+                expect(response.status).to eq 200
+            end
+
+            context "token" do
+
+                it "contains expected payload" do
+                    Timecop.freeze(Time.now)
+                    hardcoded_delay = 86400
+                    expected_payload =  {"data" => {"id" => user.id}, "expires" => (Time.now.to_i + hardcoded_delay)}
+                    
+                    post "/api/v1/auth", headers: required_headers, params: correct_body
+
+                    token = JSON.parse(response.body)["token"]
+
+                    decoded_token = JWT.decode(token, ENV['JWT_SECRET'], true, { algorithm: ENV['JWT_STRAT'] })
+
+                    expect(decoded_token.first).to eq expected_payload
+                end
+
+                it "contains the expected header" do
+                    Timecop.freeze(Time.now)
+                    expected_header =  {"alg" => ENV['JWT_STRAT']}
+                    
+                    post "/api/v1/auth", headers: required_headers, params: correct_body
+
+                    token = JSON.parse(response.body)["token"]
+                    decoded_token = JWT.decode(token, ENV['JWT_SECRET'], true, { algorithm: ENV['JWT_STRAT'] })
+
+                    expect(decoded_token.last).to eq expected_header
+                end
             end
         end
     end
